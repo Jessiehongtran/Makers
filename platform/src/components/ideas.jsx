f47fa78c9e0718ebea496d1ce569dbd4e3cc04ff
ren.meta@gmail.com <ren.meta@gmail.com>
2020-05-15 23:01:26 -0400

    defining the useStyles hook

--- platform/src/components/ideas.jsx
+++ platform/src/components/ideas.jsx
@@ -16,6 +16,7 @@ class Ideas extends React.Component {
     render(){
         return (
             <div className="ideas-frame">
+                {console.log(this.state.ideas)}
                 <button className="create-btn" onClick={() => {this.props.history.push('/create')}}>Create project</button>
                 <div className="ideas">
                     {this.state.ideas.map(idea => <Idea idea={idea} history={this.props.history}/>)}
