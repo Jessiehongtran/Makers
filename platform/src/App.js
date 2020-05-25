bb0d9525e7255cc2cd1362be442e9bcc4e4591c7
Your Name <you@example.com>
2020-05-25 16:32:35 -0700

    transformed styling of create project page

--- platform/src/App.js
+++ platform/src/App.js
@@ -60,6 +60,7 @@ render(){
             props => {
               return (
                 <> 
+                  <Nav {...props} />
                   <Create {...props}/>
                 </>
               )}}
--- platform/src/components/create.jsx
+++ platform/src/components/create.jsx
@@ -109,43 +109,56 @@ class Create extends React.Component {
                 <div className="info">
                     <h1>So what's on your mind?</h1>
                     <form>
-                        <input 
-                            placeholder="What's your idea in short" 
-                            name="idea"
-                            onChange={e => this.updateProject(e)}
-                        />
-                        <input 
-                            placeholder="Find for it a name" 
-                            name="project_name"
-                            onChange={e => this.updateProject(e)}
-                        />
+                        <div className="name">
+                            <input 
+                                placeholder="Project name" 
+                                name="project_name"
+                                onChange={e => this.updateProject(e)}
+                            />
+                            <input 
+                                placeholder="What's your idea in short" 
+                                name="idea"
+                                onChange={e => this.updateProject(e)}
+                            />
+                        </div>
+                        <div className="user">
+                            <input 
+                                placeholder="Target users" 
+                                name="target"
+                                onChange={e => this.updateProject(e)}
+                            /> 
+                            <input 
+                                placeholder="Impact" 
+                                name="impact"
+                                onChange={e => this.updateProject(e)}
+                            /> 
+                        </div>
+                        
                         {/* dropdown for category */}
-                        <input 
-                            placeholder="Category" 
-                            name="category"
-                            onChange={e => this.updateProject(e)}
-                        /> 
-                        <input 
-                            placeholder="Target users" 
-                            name="target"
-                            onChange={e => this.updateProject(e)}
-                        /> 
-                        <input 
-                            placeholder="Impact" 
-                            name="impact"
-                            onChange={e => this.updateProject(e)}
-                        /> 
-                        <input 
-                            placeholder="Team includes" 
-                            name="team"
-                            onChange={e => this.updateProject(e)}
-                        />
-                        <input 
-                            className="more-details" 
-                            placeholder="More details" 
-                            name="description"
-                            onChange={e => this.updateProject(e)}
-                        />
+                        <div className="cate-team">
+                            <input 
+                                placeholder="Category" 
+                                name="category"
+                                onChange={e => this.updateProject(e)}
+                            /> 
+                            <input 
+                                placeholder="Team includes" 
+                                name="team"
+                                onChange={e => this.updateProject(e)}
+                            />
+                        </div>
+
+                        <div className="details">
+                            <input 
+                                className="more-details" 
+                                placeholder="More details" 
+                                name="description"
+                                onChange={e => this.updateProject(e)}
+                            />
+                        </div>
+
+                        
+
                         <button 
                             onClick={() => this.submitProject()}>
                                 Create
@@ -153,72 +166,7 @@ class Create extends React.Component {
                         {/* Lead to sign in/sign up/create profile */}
                     </form>
                 </div>
-                <div className="display">
-                    {/* Using color picker input, colorhex */}
-                    {/* <div className="display-container" style={{backgroundColor: this.state.colorHex }}>                      
-                        {this.state.color}
-                    </div>
-                    <div className="display-color-picker">
-                        <input name="Color Picker" type="color" value="#e66465" onChange={e => this.colorChange(e)}/>
-                    </div>
-                    <p>---------</p> */}
-
-                    {/* Using RGB */}
-                    <div 
-                        className="display-container" 
-                        style={{
-                            backgroundColor: `#${this.fullColorHex(this.state.colorRGB.red, this.state.colorRGB.green, this.state.colorRGB.blue)}`,
-                            fontFamily: this.state.font,
-                            fontSize: this.state.fontSize,
-                        }}>                      
-                            {this.state.project.idea}
-                    </div>
-                    <div className="display-color-picker">
-                        <form>
-                            <label>
-                                Red
-                                <br/>0 <input type="number" name="red" onChange={e => this.handleColorChange(e)}/> 255
-                            </label>
-                            <label>
-                                Green
-                                <br/>0 <input type="number" name="green" onChange={e => this.handleColorChange(e)}/> 255
-                            </label>
-                            <label>
-                                Blue
-                                <br/>0 <input type="number" name="blue" onChange={e => this.handleColorChange(e)}/> 255
-                            </label>
-                        </form>
-                    </div>
-                    <div className="display-font-picker">
-                        <select name="font" onchange={e => this.updateFont(e)}>
-                            <option value="Serif"> Serif </option>
-                            <option value="Arial"> Arial </option>
-                            <option value="Sans-Serif"> Sans-Serif </option>                                  
-                            <option value="Tahoma"> Tahoma </option>
-                            <option value="Verdana"> Verdana </option>
-                            <option value="Lucida Sans Unicode"> Lucida Sans Unicode </option>
-                            <option value="'Times New Roman', Times, serif"> Times New Roman </option>    
-                            <option value="Helvetica"> Helvetica </option> 
-                            <option value="Verdana"> Verdana </option> 
-                            <option value="Garamond"> Garamond </option>  
-                            <option value="Courier"> Courier </option>    
-                            <option value="'Georgia', serif"> Georgia </option> 
-                            <option value="Palatino"> Palatino </option>
-                            <option value="Impact"> Impact </option> 
-                            <option value="Candara"> Candara </option>   
-                            <option value="Optima"> Optima </option> 
-                            <option value="Perpetua"> Perpetua </option> 
-                            <option value="Trebuchet MS"> Trebuchet MS </option> 
-                            <option value="Comic Sans"> Comic Sans </option>                               
-                        </select>
-                    </div>
-                    <div className="display-fontSize-picker">
-                        Adjust font size
-                        <button onClick={() => this.updateFontSize("down")}>-</button>
-                        {this.state.fontSize}
-                        <button onClick={() => this.updateFontSize("up")}>+</button>
-                    </div>
-                </div>
+                
             </div>
         )
     }
