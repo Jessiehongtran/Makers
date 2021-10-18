4894c0ad7b0c7dcb8683651c8942152b33c861a2
Hong Tran <jessietran@fb.com>
2021-10-18 10:16:37 -0700

    styled create page

--- platform/src/components/create2.jsx
+++ platform/src/components/create2.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import '../styles/create2.scss';
+import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
+import { faPencilAlt, faPaintBrush } from '@fortawesome/fontawesome-free-solid'
 
 export default class Create2 extends React.Component {
     constructor(props){
@@ -8,29 +11,69 @@ export default class Create2 extends React.Component {
 
     render(){
         return (
-            <div style={{width: '100%', minHeight: '100vh', display: 'flex', flexWrap: 'wrap'}}>
-                <div style={{ width: '50%', height: '50%', border: '1px solid silver' }}>
-                    <div className="title">IDEA</div>
+            <div style={{width: '100%', minHeight: '100vh', display: 'flex', flexWrap: 'wrap', padding: '20px 100px'}}>
+                <div className="piece" >
+                    <div className="info">
+                        <div className="title">IDEA</div>
+                        <div className="icons">
+                            <div className="icon pencil">
+                                <FontAwesomeIcon icon={faPencilAlt}  />
+                            </div>
+                            <div className="icon paint">
+                                <FontAwesomeIcon icon={faPaintBrush}  />
+                            </div>
+                        </div>
+                    </div>
                     <div className="space">
-                        Lorem
+                        
                     </div>
                 </div>
-                <div style={{ width: '50%', height: '50%', border: '1px solid silver' }}>
-                    <div className="title">TARGET USER</div>
+                <div className="piece" >
+                    <div className="info">
+                        <div className="title">TARGET USER</div>
+                        <div className="icons">
+                            <div className="icon pencil">
+                                <FontAwesomeIcon icon={faPencilAlt}  />
+                            </div>
+                            <div className="icon paint">
+                                <FontAwesomeIcon icon={faPaintBrush}  />
+                            </div>
+                        </div>
+                    </div>
                     <div className="space">
-                        Lorem
+                        
                     </div>
                 </div>
-                <div style={{ width: '50%', height: '50%', border: '1px solid silver' }}>
-                    <div className="title">PEOPLE</div>
+                <div className="piece" >
+                    <div className="info">
+                        <div className="title">PEOPLE</div>
+                        <div className="icons">
+                            <div className="icon pencil">
+                                <FontAwesomeIcon icon={faPencilAlt}  />
+                            </div>
+                            <div className="icon paint">
+                                <FontAwesomeIcon icon={faPaintBrush}  />
+                            </div>
+                        </div>
+                    </div>
                     <div className="space">
-                        Lorem
+                        
                     </div>
                 </div>
-                <div style={{ width: '50%', height: '50%', border: '1px solid silver' }}>
-                    <div className="title">IMPACT</div>
+                <div className="piece" >
+                    <div className="info">
+                        <div className="title">IMPACT</div>
+                        <div className="icons">
+                            <div className="icon pencil">
+                                <FontAwesomeIcon icon={faPencilAlt}  />
+                            </div>
+                            <div className="icon paint">
+                                <FontAwesomeIcon icon={faPaintBrush}  />
+                            </div>
+                        </div>
+                    </div>
                     <div className="space">
-                        Lorem
+                        
                     </div>
                 </div>
             </div>
