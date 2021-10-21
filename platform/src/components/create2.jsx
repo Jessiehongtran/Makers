fb76f0e39d786d22718aa53a9266154e0fc4bfa0
Hong Tran <jessietran@fb.com>
2021-10-21 09:46:29 -0700

    edited text component and added feature to alert user choose text vs sketch

--- platform/src/components/create2.jsx
+++ platform/src/components/create2.jsx
@@ -9,86 +9,111 @@ export default class Create2 extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            action: <Text />
+            action: <></>,
+            new_project: {
+                idea: "",
+                project_name: "",
+                category_id: 0,
+                target_user: "",
+                human_resources: "",
+                impact: "",
+                join_count: 1,
+                description: "",
+                upvote: 0
+            },
+            bgColors: [{ id: 1, color: '#fff'} , { id: 2, color: '#fff'} , { id: 3, color: '#fff'}, { id: 4, color: '#fff'}]
         }
 
         this.switchPlatform = this.switchPlatform.bind(this)
-
+        this.updateNewProject = this.updateNewProject.bind(this)
+        this.turnAttentionToIcons = this.turnAttentionToIcons.bind(this)
     }
 
-    switchPlatform(command){
+    switchPlatform(command, tag){
         if (command === "text"){
-            this.setState({ action: <Text />})
+            this.setState({ action: <Text updateNewProject={this.updateNewProject} tag={tag} />})
         }
         if (command === "sketch"){
-            this.setState({ action: <Sketch />})
+            this.setState({ action: <Sketch updateNewProject={this.updateNewProject} tag={tag} />})
         }
+    }
+
+    updateNewProject(tag, update){
+        this.setState({new_project: {...this.state.new_project, [tag]: update }})
+    }
+
+    turnAttentionToIcons(ind){
+        let { bgColors } = this.state
+        bgColors.forEach(bg => bg.id === ind ? bg.color = '#FBD6D6' : bg.color= '#fff')
+        this.setState({ bgColors: bgColors })
     }
 
     render(){
+
+
         return (
             <div style={{width: '100%', minHeight: '100vh', display: 'flex', flexWrap: 'wrap', padding: '20px 100px'}}>
-                <div className="piece" >
+                <div className="piece" onMouseOver={() => this.turnAttentionToIcons(1)}>
                     <div className="info">
                         <div className="title">IDEA</div>
                         <div className="icons">
-                            <div className="create-icon pencil" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil" onClick={() => this.switchPlatform('text', 'idea')} style={{backgroundColor: `${this.state.bgColors[0].color}`}}>
                                 <FontAwesomeIcon icon={faPencilAlt}  />
                             </div>
-                            <div className="create-icon paint">
-                                <FontAwesomeIcon icon={faPaintBrush}  onClick={() => this.switchPlatform('sketch')}/>
+                            <div className="create-icon paint" onClick={() => this.switchPlatform('sketch', 'idea')} style={{backgroundColor: `${this.state.bgColors[0].color}`}}>
+                                <FontAwesomeIcon icon={faPaintBrush}  />
                             </div>
                         </div>
                     </div>
-                    <div className="space">
+                    <div className="space" >
                         {this.state.action}
                     </div>
                 </div>
-                <div className="piece" >
+                <div className="piece" onMouseOver={() => this.turnAttentionToIcons(2)}>
                     <div className="info">
                         <div className="title">TARGET USER</div>
                         <div className="icons">
-                            <div className="icon pencil" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'target_user')} style={{backgroundColor: `${this.state.bgColors[1].color}`}}>
                                 <FontAwesomeIcon icon={faPencilAlt}  />
                             </div>
-                            <div className="icon paint" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'target_user')} style={{backgroundColor: `${this.state.bgColors[1].color}`}}>
                                 <FontAwesomeIcon icon={faPaintBrush}  />
                             </div>
                         </div>
                     </div>
-                    <div className="space">
+                    <div className="space" >
                         {this.state.action}
                     </div>
                 </div>
-                <div className="piece" >
+                <div className="piece" onMouseOver={() => this.turnAttentionToIcons(3)} >
                     <div className="info">
                         <div className="title">PEOPLE</div>
                         <div className="icons">
-                            <div className="icon pencil" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'human_resources')} style={{backgroundColor: `${this.state.bgColors[2].color}`}}>
                                 <FontAwesomeIcon icon={faPencilAlt}  />
                             </div>
-                            <div className="icon paint" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'human_resources')} style={{backgroundColor: `${this.state.bgColors[2].color}`}}>
                                 <FontAwesomeIcon icon={faPaintBrush}  />
                             </div>
                         </div>
                     </div>
-                    <div className="space">
+                    <div className="space" >
                         {this.state.action}
                     </div>
                 </div>
-                <div className="piece" >
+                <div className="piece" onMouseMove={() => this.turnAttentionToIcons(4)}>
                     <div className="info">
                         <div className="title">IMPACT</div>
                         <div className="icons">
-                            <div className="icon pencil" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'impact')} style={{backgroundColor: `${this.state.bgColors[3].color}`}}>
                                 <FontAwesomeIcon icon={faPencilAlt}  />
                             </div>
-                            <div className="icon paint" onClick={() => this.switchPlatform('text')}>
+                            <div className="create-icon pencil"  onClick={() => this.switchPlatform('text', 'impact')} style={{backgroundColor: `${this.state.bgColors[3].color}`}}>
                                 <FontAwesomeIcon icon={faPaintBrush}  />
                             </div>
                         </div>
                     </div>
-                    <div className="space">
+                    <div className="space" >
                         {this.state.action}
                     </div>
                 </div>
--- platform/src/components/text.jsx
+++ platform/src/components/text.jsx
@@ -6,9 +6,15 @@ export default class Text extends React.Component {
     }
 
     render(){
+
         return (
-            <div>
-                Text
+            <div style={{marginTop: '10px', height: '100%'}}>
+                <textarea 
+                    style={{ border: 'none', outline: 'none', width: '100%', padding: '10px 20px', height: '50vh', backgroundColor: "", resize: 'none'}}
+                    className="idea-input" 
+                    placeholder="..."
+                    onChange={(e) => this.props.updateNewProject(this.props.tag, e.target.value)}
+                ></textarea>
             </div>
         )
     }
